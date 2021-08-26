6eba96767b9e856d3637f2e6c02a9dd7cde474e1
Tess <68465860+HelixDev@users.noreply.github.com>
2021-08-26 10:48:38 +0200

    Add files via upload

---
+++ OpenCrypt API/admin.py
@@ -0,0 +1,26 @@
+import requests
+def main():
+    prnt()
+    print("Insert the URL: (http://website.website/dir)")
+    url = input("> ")
+    prnt()
+    print("Insert admin key")
+    token = input("> ")
+    prnt()
+    print("Insert new auth key")
+    key = input("> ")
+    requests.post('{url}/admin.php?ADMIN_TOKEN={token}?key={key}')
+def prnt():
+    var = """
+    ###############################################
+    #                                             #
+    #         OPENCRYPT API ADMIN PORTAL          #
+    #                                             #
+    #              HELIXDEV 2021                  #
+    #                                             #
+    ###############################################
+    """
+    print("\n"*100)
+    print(var)
+if __name__ == '__main__':
+    main()
